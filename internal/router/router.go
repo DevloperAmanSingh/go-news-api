9a718aacfcb98e404f69eb539b582ca4e15ef0e1
AMAN SINGH <amansingh@AMANs-MacBook-Air.local>
2024-04-08 23:17:59 +0530

    Added Comment and stories fetching

---
+++ internal/router/router.go
@@ -0,0 +1,29 @@
+package router
+
+import (
+	"github.com/DevloperAmanSingh/go-news-api/internal/controllers"
+	"github.com/DevloperAmanSingh/go-news-api/internal/handlers"
+	"github.com/gofiber/fiber/v2"
+)
+
+func SetupRouter() *fiber.App {
+	app := fiber.New()
+
+	// Define routes
+
+	app.Get("/api/stories/:id/comments", func(c *fiber.Ctx) error {
+		return handlers.GetStoryComments(c)
+	})
+	app.Post("/register", func(c *fiber.Ctx) error {
+		return controllers.SignUp(c)
+	})
+
+	app.Post("/login", func(c *fiber.Ctx) error {
+		return controllers.Login(c)
+	})
+
+	app.Get("/api/users/:id", func(c *fiber.Ctx) error {
+		return handlers.GetUserFromApi(c)
+	})
+	return app
+}
